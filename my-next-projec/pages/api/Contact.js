239115a3180d051e7ab4adae54a7a4a6f14c457e
Anass-za <99453408+Anass-za@users.noreply.github.com>
2023-01-02 14:59:11 +0000

    changed mail

--- my-next-projec/pages/api/Contact.js
+++ my-next-projec/pages/api/Contact.js
@@ -3,7 +3,7 @@ javascript
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
-  to: 'zahir.electro.tech@gmailcom', // Change to your recipient
+  to: 'anass123zahir@gmailcom', // Change to your recipient
   from: 'zahir.electro.tech@gmail.com', // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
