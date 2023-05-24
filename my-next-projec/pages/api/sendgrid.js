6580c9f4611b6b55581a556a41cd100c9e5d55ad
Anass-za <99453408+Anass-za@users.noreply.github.com>
2023-05-24 22:00:13 +0100

    Update sendgrid.js

--- my-next-projec/pages/api/sendgrid.js
+++ my-next-projec/pages/api/sendgrid.js
@@ -1,48 +1,44 @@
-html,
-body {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    background: #eee;
-}
+import sendgrid from "@sendgrid/mail";
 
-a {
-  color: inherit;
-  text-decoration: none;
-}
+sendgrid.setApiKey(process.env.SEND_GRID_TOKEN);
 
-* {
-  box-sizing: border-box;
-}
+export default async (req, res) => {
+  try {
+    await sendgrid.send({
+      to: "anass123zahir@gmail.com",
+      from: "zahir.electro.tech@gmail.com",
+      subject: "Contact form email",
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
 
-.container {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 20px;
-  background: white;
-  border-radius: 10px;
-  box-shadow: 1px 2px 3px #ddd;
-}
-h2, h3 {
-  text-align: center;
-}
-.input_container {
-  margin-bottom: 20px;
-}
-.input {
-  width: 100%;
-  padding: 5px;
-  outline: none;
-}
-.btn_container {
-  text-align: center;
-}
-button {
-  background: #333;
-  color: white;
-  padding: 5px 10px;
-  border-radius: 5px;
-  outline: none;
-  font-size: 16px;
-}
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+      </head>
+
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <b>From:</b> ${req.body.name} <br /> 
+              <b>Email:</b> ${req.body.email} <br />
+              <b>Number:</b> ${req.body.number} <br /> 
+              <b>Subject:</b> ${req.body.subject} <br /> 
+              <b>Message:</b> ${req.body.text} 
+              </div>
+              </div>
+      </body>
+      </html>`,
+    });
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+
+  return res.status(200).json({ error: "" });
+};
