84e30fe1c4687e158311ba192a171b5a09049839
Anass-za <99453408+Anass-za@users.noreply.github.com>
2023-06-21 14:01:57 +0100

    Update sendgrid.js

--- my-next-projec/pages/api/sendgrid.js
+++ my-next-projec/pages/api/sendgrid.js
@@ -1,44 +1,27 @@
-import sendgrid from "@sendgrid/mail";
-
-sendgrid.setApiKey(process.env.SEND_GRID_TOKEN);
-
-export default async (req, res) => {
-  try {
-    await sendgrid.send({
-      to: "anass123zahir@gmail.com",
-      from: "zahir.electro.tech@gmail.com",
-      subject: "Contact form email",
-      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-      <html lang="en">
-      <head>
-        <meta charset="utf-8">
-
-        <title>The HTML5 Herald</title>
-        <meta name="description" content="The HTML5 Herald">
-        <meta name="author" content="SitePoint">
-      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-
-        <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-      </head>
-
-      <body>
-        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
-              </div>
-              <div class="container" style="margin-left: 20px;margin-right: 20px;">
-              <b>From:</b> ${req.body.name} <br /> 
-              <b>Email:</b> ${req.body.email} <br />
-              <b>Number:</b> ${req.body.number} <br /> 
-              <b>Subject:</b> ${req.body.subject} <br /> 
-              <b>Message:</b> ${req.body.text} 
-              </div>
-              </div>
-      </body>
-      </html>`,
-    });
-  } catch (error) {
-    return res.status(error.statusCode || 500).json({ error: error.message });
+export default function handler(req, res) {  
+    if(req.method === 'POST') {
+      const sgMail = require('@sendgrid/mail');
+      sgMail.setApiKey("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:"); //SendGridのAPIキー
+       
+      const msg = {
+        to: req.body.email,
+        from: 'support@example.com',
+        subject: 'お問合せありがとうございました。',
+        text: 'お問合せを受け付けました。回答をお待ちください。' + req.body.message,
+        html: 'お問合せを受け付けました。回答をお待ちください。' + req.body.message,
+      };
+   
+      (async () => {
+        try {
+          await sgMail.send(msg);
+        } catch (error) {
+          console.error(error);
+          if (error.response) {
+            console.error(error.response.body)
+          }
+        }
+      })();
+    }
+   
+    res.status(200)
   }
-
-  return res.status(200).json({ error: "" });
-};
